775cb0c4f9242d0a8385313814ef587cb06bf35b
Carrie Kam <36577595+CarrieKam@users.noreply.github.com>
2023-01-29 23:03:15 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,17 +5,15 @@ import Layout from '../components/layout'
 const AboutPage = () => {
     return (
         <Layout pageTitle="About Me">
-            <p>I am a Software Engineering at Polytechnique de Montreal.</p>
+            <p>I am a Software Engineering student at Polytechnique de Montreal.</p>
+            <p>During the summer of 2022, I got accepted to be a MLH Fellow in the Product Engineering program. 
+                I was very honored for that. I learned a lot of things and made some friends. I took notes for 
+                everything that I learned so that I can't forget them in the future. </p>
+            
             <p>During the summer of 2021, I decided to join my friend at the event INIT2022
                 to learn more about coding. We created a guild and thought it was the same
                 thing as a group. </p>
             
-            <p>When the event started, we realized that everyone could
-                join and we had the responsibility to entertain the members and enjoy the
-                whole event. We created a discord server later on and talked to some people
-                in the guild. There was a challenge that we needed to draw on Ms Paint a famous
-                painting. We shared our paintings we the rest of the guild and found out that
-                there are a lot of talented artists.</p>
             <StaticImage
                 alt="init2020"
                 src="../images/init2022.png"
